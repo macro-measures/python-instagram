197013de12ded3b5eac2b97f4e6d146b9b0e4fc9
Wendy Liu <ilostwaldo@gmail.com>
2017-09-28 13:47:34 +0100

    Handle TypeError when fetching post data
    getting `TypeError: __init__() got an unexpected keyword argument 'id'` when calling `new_media.videos[version] = Video.object_from_dictionary(version_info)` for certain users (e.g., 440639027)

--- instagram/models.py
+++ instagram/models.py
@@ -84,6 +84,8 @@ class Media(ApiModel):
         if new_media.type == 'video' and 'videos' in entry:
             new_media.videos = {}
             for version, version_info in six.iteritems(entry['videos']):
+                if 'id' in version_info:
+                    del version_info['id']
                 new_media.videos[version] = Video.object_from_dictionary(version_info)
 
         if 'user_has_liked' in entry:
